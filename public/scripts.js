53f4042927c8a8a350ab7ce21735f4e0d627ab51
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-05-27 18:35:00 -0300

    Refatorando FoodFy - Layout da página da receita ok, falta função esconder

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,28 @@
 const cards = document.querySelectorAll(".card");
 
 for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function() {
+    cards[i].addEventListener("click", function () {
         window.location.href = `/recipes/${i}`
     })
 }
+
+const button = document.getElementsByClassName(".show-hide");
+button.addEventListener("click", function(id) {
+    if (document.getElementById(id).style.display == "block") {
+        document.getElementById(id).style.display = "none"
+        document.getElementById(id).value = "MOSTRAR"
+    } else {
+        document.getElementById(id).style.display = "block"
+        document.getElementById(id).value = "ESCONDER"
+    }
+})
+
+// function mostrar(id) {
+//     if (document.getElementById(id).style.display == "block") {
+//         document.getElementById(id).style.display = "none"
+//         document.getElementById(id).value = "MOSTRAR"
+//     } else {
+//         document.getElementById(id).style.display = "block"
+//         document.getElementById(id).value = "ESCONDER"
+//     }
+// }
