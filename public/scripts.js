3c5f627a293e9494a7acdcd882668cce20e0d73f
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-05-28 11:23:59 -0300

    Refatorando FoodFy - Função esconder

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll(".card");
+const buttons = document.getElementsByClassName("show-hide");
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
@@ -6,23 +7,15 @@ for (let i = 0; i < cards.length; i++) {
     })
 }
 
-const button = document.getElementsByClassName(".show-hide");
-button.addEventListener("click", function(id) {
-    if (document.getElementById(id).style.display == "block") {
-        document.getElementById(id).style.display = "none"
-        document.getElementById(id).value = "MOSTRAR"
-    } else {
-        document.getElementById(id).style.display = "block"
-        document.getElementById(id).value = "ESCONDER"
-    }
-})
-
-// function mostrar(id) {
-//     if (document.getElementById(id).style.display == "block") {
-//         document.getElementById(id).style.display = "none"
-//         document.getElementById(id).value = "MOSTRAR"
-//     } else {
-//         document.getElementById(id).style.display = "block"
-//         document.getElementById(id).value = "ESCONDER"
-//     }
-// }
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (event) => {
+        const id = (event.target).getAttribute("data-target")
+        if (document.getElementById(id).style.display == "block") {
+            document.getElementById(id).style.display = "none"
+            event.target.innerHTML = "MOSTRAR"
+        } else {
+            document.getElementById(id).style.display = "block"
+            event.target.innerHTML = "ESCONDER"
+        }
+    })
+}
